17fa312844537bf979319e0ca0e949c31ad5dfb8
BrennanScheel <brennanscheel@yahoo.com>
2024-04-08 16:18:18 -0500

    Added order placement

--- Brennans controllers/OrderStatusController.cs
+++ Brennans controllers/OrderStatusController.cs
@@ -41,6 +41,16 @@ namespace Group8.Controllers
             return View();
         }
 
+        public ActionResult PlaceOrder()
+        {
+            return View();
+        }
+
+        public ActionResult ConfirmOrder()
+        {
+            return View();
+        }
+
         // POST: OrderStatus/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
